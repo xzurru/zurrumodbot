42787a9a5c9ecd95d5b6b64c81f11149a4bddf88
xzurru <107719241+xzurru@users.noreply.github.com>
2025-01-10 19:39:22 +0100

    Create welcome.py

---
+++ cogs/welcome.py
@@ -0,0 +1,35 @@
+import discord
+from discord.ext import commands
+
+class Welcome(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.welcome_channel_id = channel id
+        self.role1_id = role id
+        self.role2_id = role id
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        """Executes when a member joins the server."""
+        channel = self.bot.get_channel(self.welcome_channel_id)
+        role1 = member.guild.get_role(self.role1_id)
+        role2 = member.guild.get_role(self.role2_id)
+
+        if role1:
+            await member.add_roles(role1)
+        if role2:
+            await member.add_roles(role2)
+
+        if channel:
+            embed = discord.Embed(
+                description=f"Welcome {member.mention}! Welcome, If you have any questions, use our support system. Have fun on the Discord!",
+                color=discord.Color.green()
+            )
+            if member.avatar:
+                embed.set_thumbnail(url=member.avatar.url)
+            else:
+                embed.set_thumbnail(url="https://via.placeholder.com/150")
+            await channel.send(embed=embed)
+
+async def setup(bot):
+    await bot.add_cog(Welcome(bot))
